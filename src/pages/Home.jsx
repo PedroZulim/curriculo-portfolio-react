f112dc0b3158cbbde16652467b3a2ea3b96a7503
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2025-08-05 14:26:46 -0300

    adjust home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Home() {
   return (
     <div style={{ textAlign: "center", padding: "2rem" }}>
-      <h1>Meus Currículos</h1>
+      <h1>Currículos</h1>
       <p>Escolha qual currículo deseja visualizar:</p>
       <ul style={{ listStyle: "none", padding: 0 }}>
         <li><Link to="/PedroZulim">Currículo Pedro</Link></li>
