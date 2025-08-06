282bbb7421ba35e1004cd941bdeda2cf149da0ce
Pedro Henrique de Souza Zulim <pedrozulim2@gmail.com>
2025-08-05 23:13:19 -0300

    hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Curriculo1 from "./pages/CurriculoPedro";
 import Curriculo2 from "./pages/CurriculoAna";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/curriculo-portfolio-react/" element={<Home />} />
         <Route path="/curriculo-portfolio-react/PedroZulim" element={<Curriculo1 />} />
         <Route path="/curriculo-portfolio-react/AnaJulia" element={<Curriculo2 />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
